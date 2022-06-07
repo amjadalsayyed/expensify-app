851964250d4b8e515756b73fe3e15add7ead11de
amjadalsayyed <amgad.alsayyed@yahoo.com>
2022-06-07 17:47:18 +0200

    remove dummy expense date

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import getvisible from "./selectorrs/expenses";
 
 const store = configstore();
 
-store.dispatch(addExpense({ description: "water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "gas bill", craetedAt: 1000 }));
-store.dispatch(addExpense({ description: "rent", amount: 109500 }));
-// store.dispatch(setTextFilter("bill"));
-const state = store.getState();
-
-console.log(getvisible(state.expenses, state.filters));
 const jsx = (
   <Provider store={store}>
     <AppRouter />
